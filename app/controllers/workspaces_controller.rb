0a0fc5de5c220af528fca1a9678d1517802c139f
deng gong <rzpfox@gmail.com>
2018-11-27 17:27:23 +0800

    added view for devise

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,4 +1,6 @@
 class WorkspacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @workspaecs = Workspace.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 #  get 'reviews/update'
 #  get 'reviews/destroy'
   devise_for :users
-  root to: "home#index"
+  root to: "workspaces#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :workspaces do
     resources :reviews, shallow: true
